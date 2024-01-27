1cd3cf470ddedc0e977fa13dc7666a54e3bc859d
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-26 20:13:59 -0500

    adds thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+// GET and POST routes to get all thoughts and create a new thought
+router.route("/")
+    // GET: /api/thoughts
+    .get(getAllThoughts)
+    // POST: /api/thoughts
+    .post(createThought);
+
+
+// GET, PUT, DELETE routes by thoughts id: /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
+
+module.exports = router;
